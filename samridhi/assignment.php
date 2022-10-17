ddb87ccc2d876447093e8d53f5d6182014363d7d
samridhi0 <goyalsamridhi062000@gmail.com>
2022-10-17 20:24:17 +0530

    assignment.php

---
+++ samridhi/assignment.php
@@ -0,0 +1,48 @@
+<html>
+<body style="background-color: #ffccff;">
+<h1 style="text-align:center;color:#660066;font-size:300%;">ASSIGNMENT OPERATOR</h1>
+<h4 style=:text-align:center;color:#330033;">assignment</h4>
+<?php
+$x = 10;  
+echo $x;
+?> 
+ 
+<h4 style=:text-align:center;color:#330033;">addition</h4>
+<?php
+$x = 120;  
+$x += 100;
+echo $x;
+?> 
+ 
+<h4 style=:text-align:center;color:#330033;">subtraction</h4>
+<?php
+$x = 150;
+$x -= 20;
+echo $x;
+?>  
+
+<h4 style=:text-align:center;color:#330033;">Multiplication</h4>
+<?php
+$x = 5;
+$x *= 6;
+
+echo $x;
+?>  
+
+<h4 style=:text-align:center;color:#330033;">Division</h4>
+<?php
+$x = 150;
+$x /= 20;               
+echo $x;
+?>  
+
+<h4 style=:text-align:center;color:#330033;">Modulus</h4>
+<?php
+$x = 150;
+$x %= 20;
+
+echo $x;
+?>  
+
+</body>
+</html>
