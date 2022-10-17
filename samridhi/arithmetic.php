0ae3385c7642f0e4b377a620667aef818fc18252
samridhi0 <goyalsamridhi062000@gmail.com>
2022-10-17 09:43:30 +0530

    arithmetic.php

---
+++ samridhi/arithmetic.php
@@ -0,0 +1,56 @@
+<html>
+<body>
+<h1 style="text-align:center;font-size:300%;">operators</h1>
+<h2>ADDITION</h2>
+<?php
+$x = 5;  
+$y = 16;
+
+echo $x + $y;
+?>  
+<br>
+<h2>SUBTRACTION</h2>
+<?php
+$x = 6;  
+$y = 16;
+
+echo $x - $y;
+?>  
+<br>
+<h2>MULTIPLICATION</h2>
+<?php
+$x = 11;  
+$y = 6;
+
+echo $x * $y;
+?>  
+
+<br>
+<h2>DIVISIONS</h2>
+<?php
+$x = 50;  
+$y = 6;
+
+echo $x / $y;
+?>  
+
+<br>
+<h2>MODULUS</h2>
+<?php
+$x = 50;  
+$y = 11;
+echo $x % $y;
+?> 
+ 
+
+<br>
+<h2>EXPONENTIATION</h2>
+<?php
+$x = 11;  
+$y = 11;
+echo $x ** $y;
+?>  
+
+
+</body>
+</html>
